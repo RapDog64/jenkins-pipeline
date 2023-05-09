41effc64a21b68a853b2a1e747aebf5d4a40ec29
Denis Kolovorotnyi <denis.kolovorotnyi@septhora.com>
2023-05-09 12:40:25 +0200

    Added random change the tests to be passed

--- src/test/java/com/pipleline/RunTestByTags.java
+++ src/test/java/com/pipleline/RunTestByTags.java
@@ -3,10 +3,11 @@ package com.pipleline;
 
 import io.qameta.allure.Allure;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 @Tag("regression")
 class RunTestByTags {
@@ -25,6 +26,10 @@ class RunTestByTags {
     @DisplayName("Test with 'sanity' tag")
     void sanityTest() {
         Allure.step("Test with 'sanity' tag");
+        Random random = new Random();
+        int number = random.nextInt(10);
+        System.out.println(number);
+        Assertions.assertTrue(number > 5);
     }
 
 
@@ -33,6 +38,9 @@ class RunTestByTags {
     @DisplayName("The Second test with 'smoke' tag")
     void secondSmokeTest() {
         Allure.step("The Second test with 'smoke' tag");
-        Assertions.assertEquals(4, 3+2);
+        Random random = new Random();
+        int number = random.nextInt(10);
+        System.out.println(number);
+        Assertions.assertTrue(number > 5);
     }
 }
